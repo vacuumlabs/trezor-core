8b77a89881af51b24dfad62478f6cad679811cc6
Tomas Susanka <tsusanka@gmail.com>
2018-05-11 15:31:53 +0200

    stellar: init; get public key

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ import apps.wallet
 import apps.ethereum
 import apps.lisk
 import apps.nem
+import apps.stellar
 if __debug__:
     import apps.debug
 else:
@@ -25,6 +26,7 @@ apps.wallet.boot()
 apps.ethereum.boot()
 apps.lisk.boot()
 apps.nem.boot()
+apps.stellar.boot()
 if __debug__:
     apps.debug.boot()
 else:
